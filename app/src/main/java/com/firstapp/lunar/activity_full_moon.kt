f1364b478535be282c289e3f24702c64289ac15e
Maja <57007921+mjrjk@users.noreply.github.com>
2020-04-11 18:47:43 +0200

    Added increasing and decreasing buttons

--- app/src/main/java/com/firstapp/lunar/activity_full_moon.kt
+++ app/src/main/java/com/firstapp/lunar/activity_full_moon.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_full_moon.*
 
 class activity_full_moon : AppCompatActivity() {
@@ -13,7 +14,31 @@ class activity_full_moon : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_moon)
 
+        increase()
+        decrease()
     }
 
+    private fun increase(){
+
+        val buttonPlus: Button = findViewById(R.id.buttonPlus)
+
+        buttonPlus.setOnClickListener(){
+        val editText: EditText = findViewById(R.id.editText)
+            val year = editText.text.toString()
+            val increased = year.toInt() + 1
+        editText.setText(increased.toString())
+        }
+    }
+
+    private fun decrease(){
+        val buttonMinus: Button = findViewById(R.id.buttonMinus)
+
+        buttonMinus.setOnClickListener(){
+            val editText: EditText = findViewById(R.id.editText)
+            val year = editText.text.toString()
+            val decreased = year.toInt() - 1
+            editText.setText(decreased.toString())
+        }
+    }
 
 }
